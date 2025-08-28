6c592da57b2e062ed46bcba3abadaf0eaf7a9b1a
NataB <nataballesterossierra@gmail.com>
2025-08-28 12:58:06 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,17 @@ public class Main {
         System.out.println("Hello class");
 
         System.out.println("we are learning GIT");
-        System.out.println("1.COMANDOS");
-        System.out.println("2. git status");
-        System.out.println("3. git add . ");
-        System.out.println("4. git commit -m .cambios. ");
-        System.out.println("5. git push origin main");
-        System.out.println("6. git clone");
-        System.out.println("7. git branch");
-        System.out.println("8. git fetch -v");
-        System.out.println("9. git switch -c");
+        System.out.println("COMANDOS");
+        System.out.println("2. git status - ESTADO RAMA");
+        System.out.println("3. git add . - AGREGAR ARCHIVOS");
+        System.out.println("4. git commit -m .cambios.- GUARDAR LOCAL");
+        System.out.println("5. git push origin - SUBIR CAMBIOS");
+        System.out.println("6. git clone - CLONAR PROYECTO");
+        System.out.println("7. git branch - VER LAS RAMAS");
+        System.out.println("8. git fetch -v - RAMAS REMOTAS");
+        System.out.println("9. git switch -c - CREAR Y POSISCIONARME EN UNA RAMA");
         System.out.println("10. git origin desarrollojava");
+        System.out.println("11. git checkout ");
+        System.out.println("git pull origin ");
     }
 }
