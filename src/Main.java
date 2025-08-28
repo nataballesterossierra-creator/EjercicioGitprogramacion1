a02bca1322fead1202d3db6284828aab52b37948
NataB <nataballesterossierra@gmail.com>
2025-08-28 12:45:10 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,19 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("Hello class");
+
+        System.out.println("we are learning GIT");
+        System.out.println("1.COMANDOS");
+        System.out.println("2. git status");
+        System.out.println("3. git add . ");
+        System.out.println("4. git commit -m .cambios. ");
+        System.out.println("5. git push origin main");
+        System.out.println("6. git clone");
+        System.out.println("7. git branch");
+        System.out.println("8. git fetch -v");
+        System.out.println("9. git switch -c");
+        System.out.println("10. git origin desarrollojava");
     }
 }
